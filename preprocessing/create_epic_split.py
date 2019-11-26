109c4dae2d67bc7f3d1233e7f43a5f0d167b1fde
Tridivraj Bhattacharyya <tridiv_b@yahoo.com>
2019-11-26 15:14:45 +0100

    Add script for trainval split

---
+++ preprocessing/create_epic_split.py
@@ -0,0 +1,55 @@
+import os
+import argparse
+import random
+import pandas as pd
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="create dataset split")
+    parser.add_argument(
+        "annotation",
+        help="annoation file to read",
+        type=str,
+    )
+    parser.add_argument(
+        "--out_dir",
+        help="output directory",
+        dest="out_dir",
+        default="./",
+        type=str,
+    )
+    return parser.parse_args()
+
+def write_list_to_file(file, lst):
+    with open(file, "w") as f:
+        for item in sorted(lst):
+            f.write("%s\n"%item)
+
+def create_split(args):
+
+    if args.annotation.endswith("csv"):
+        df = pd.read_csv(args.annotation)
+    elif args.annotation.endswith("pkl"):
+        df = pd.read_pickle(args.annotation)
+    else:
+        raise Exception("Incorrect file extension for annotation file. Must be a csv or pkl file")
+
+    train_list = []
+    val_list = []
+
+    for p_id in df.participant_id.unique():
+        data = df.query("participant_id == @p_id")
+        vid_ids = list(data.video_id.unique())
+        random.shuffle(vid_ids)
+        train_list.extend(vid_ids[:-1])
+        val_list.append(vid_ids[-1])
+
+    train_list_file = os.path.join(args.out_dir, "train_split.txt")
+    val_list_file = os.path.join(args.out_dir, "val_split.txt")
+
+    write_list_to_file(train_list_file, train_list)
+    write_list_to_file(val_list_file, val_list)
+
+if __name__ == "__main__":
+    
+    args = parse_args()
+    create_split(args)
