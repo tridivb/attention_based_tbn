423b75328d9fde8d60cc2614a7d541e84b41f3a0
Tridivraj Bhattacharyya <tridiv_b@yahoo.com>
2019-11-29 17:54:43 +0100

    Separate class for metrics

---
+++ utils/metric.py
@@ -0,0 +1,51 @@
+import torch
+import numpy as np
+
+
+class Metric(object):
+    def __init__(self):
+        super(Metric, self).__init__()
+        pass
+
+    def calculate_metrics(self, out, target, topk=[1,]):
+        maxk = max(topk)
+        batch_size = out.size(0)
+        conf_mat = np.zeros((out.size(1), out.size(1)))
+
+        if target.size(0) < batch_size:
+            assert batch_size % target.size(0) == 0
+            target = target.repeat(int(batch_size / target.size(0)))
+
+        _, preds = out.topk(maxk, 1, True, True)
+        preds = preds.t()
+        correct = preds.eq(target.view(1, -1).expand_as(preds))
+
+        for i1, i2 in zip(target.view(-1), preds[0, :].view(-1)):
+            conf_mat[i1, i2] += 1
+
+        acc = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).to(torch.float32).sum(0)
+            acc.append(float(correct_k.mul_(100.0 / batch_size)))
+
+        precision, recall = self.get_precision_recall(conf_mat)
+
+        return acc, conf_mat, precision, recall
+
+    @staticmethod
+    def get_precision_recall(conf_mat):
+        precision = 0
+        recall = 0
+
+        tn = conf_mat[0, 0]
+        tp = np.trace(conf_mat[1:, 1:])
+        fn = conf_mat[:, 0].sum() - tn
+        fp = conf_mat.sum() - tp - fn - tn
+
+        if tp + fp > 0:
+            precision = tp / (tp + fp)
+
+        if tp + fn > 0:
+            recall = tp / (tp + fn)
+
+        return precision, recall
--- utils/misc.py
+++ utils/misc.py
@@ -1,23 +1,5 @@
 import torch
-import torchvision
 import numpy as np
-import time
-from collections import OrderedDict
-
-
-def calculate_topk_accuracy(out, target, topk=[1,]):
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, preds = out.topk(maxk, 1, True, True)
-    preds = preds.t()
-    correct = preds.eq(target.view(1, -1).expand_as(preds))
-
-    acc = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).to(torch.float32).sum(0)
-        acc.append(float(correct_k.mul_(100.0 / batch_size)))
-    return acc
 
 
 def get_time_diff(start_time, end_time):
@@ -27,11 +9,13 @@ def get_time_diff(start_time, end_time):
     return (hours, minutes, seconds)
 
 
-def save_checkpoint(model, optimizer, epoch, filename="checkpoint.pth"):
-    torch.save(
-        {
-            "epoch": epoch,
-            "model": model.state_dict(),
-            "optimizer": optimizer.state_dict(),
-        }
-    )
+def save_checkpoint(model, optimizer, epoch, scheduler=None, filename="checkpoint.pth"):
+    data = {
+        "epoch": epoch,
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    if scheduler:
+        data["scheduler"] = scheduler.state_dict()
+
+    torch.save(data, filename)
