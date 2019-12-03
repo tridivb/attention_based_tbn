acee32c2d7f3569c00e19b465abe85f3c4712cba
Tridivraj Bhattacharyya <tridiv_b@yahoo.com>
2019-12-03 14:25:23 +0100

    Add relu, softmax to fusion and classification layers

--- models/model.py
+++ models/model.py
@@ -17,6 +17,11 @@ class TBNModel(nn.Module):
         self.modality = modality
         self.base_model_name = cfg.MODEL.ARCH
         self.num_classes = cfg.MODEL.NUM_CLASSES
+        if cfg.MODEL.AGG_TYPE.lower() == "avg":
+            self.agg_type = "avg"
+        else:
+            print("Incorrect aggregation type")
+            self.agg_type = None
 
         in_features = 0
         for m in self.modality:
@@ -26,10 +31,20 @@ class TBNModel(nn.Module):
                 self._tune_base_model(modality, requires_grad=False)
 
         if len(self.modality) > 1:
-            self.add_module("fusion_layer", self._create_fusion_layer(in_features))
-            self.add_module("classifier", Classifier(self.num_classes, 512))
+            self.add_module(
+                "fusion", Fusion(in_features, 512, dropout=cfg.MODEL.FUSION_DROPOUT)
+            )
+            self.add_module(
+                "classifier",
+                Classifier(self.num_classes, 512, use_softmax=cfg.MODEL.USE_SOFTMAX),
+            )
         else:
-            self.add_module("classifier", Classifier(self.num_classes, in_features))
+            self.add_module(
+                "classifier",
+                Classifier(
+                    self.num_classes, in_features, use_softmax=cfg.MODEL.USE_SOFTMAX
+                ),
+            )
 
     def _create_base_model(self, modality):
         if modality == "RGB":
@@ -48,12 +63,21 @@ class TBNModel(nn.Module):
 
         return base_model
 
-    def _create_fusion_layer(self, in_features):
-        return nn.Linear(in_features, 512)
-
     def _tune_base_model(self, modality, requires_grad=False):
         for param in getattr(self, modality):
             param.requires_grad = requires_grad
+
+    def _aggregate_scores(self, scores, new_shape=(1, -1)):
+        assert isinstance(scores, (dict, torch.tensor))
+        assert isinstance(new_shape, tuple)
+
+        if isinstance(scores, dict):
+            for key in scores.keys():
+                scores[key] = scores[key].view(new_shape).mean(dim=1)
+        else:
+            scores = scores[key].view(new_shape).mean(dim=1)
+
+        return scores
 
     def forward(self, input):
         features = []
@@ -64,10 +88,12 @@ class TBNModel(nn.Module):
             features.extend([feature.view(b * n, -1)])
         features = torch.cat(features, dim=1)
 
-        if self.fusion_layer:
-            features = self.fusion_layer(features)
+        if self.fusion:
+            features = self.fusion(features)
 
         out = self.classifier(features)
+
+        out = self._aggregate_scores(out, new_shape=(b, n, -1))
 
         return out
 
@@ -78,28 +104,53 @@ class TBNModel(nn.Module):
         loss = 0
 
         for key in target.keys():
-            if target[key].shape[0] < preds[key].shape[0]:
-                assert preds[key].shape[0] % target[key].shape[0] == 0
-                labels = target[key].repeat_interleave(preds[key].shape[0] // target[key].shape[0])
-            else:
-                labels = target
+            labels = target[key]
             loss += criterion(preds[key], labels)
 
         return loss
 
 
+class Fusion(nn.Module):
+    def __init__(self, in_size, out_size, dropout=0):
+        super(Fusion, self).__init__()
+
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dropout = dropout
+
+        self.fusion_layer = nn.Sequential(nn.Linear(in_size, out_size), nn.ReLU())
+
+        if self.dropout > 0:
+            self.dropout_layer = nn.Dropout(p=self.dropout)
+
+    def forward(self, input):
+        out = self.fusion_layer(input)
+
+        if self.dropout > 0:
+            out = self.dropout_layer(out)
+
+        return out
+
+
 class Classifier(nn.Module):
-    def __init__(self, num_classes, in_features):
+    def __init__(self, num_classes, in_features, use_softmax=False):
         super(Classifier, self).__init__()
 
         for class_name in num_classes.keys():
             self.add_module(class_name, nn.Linear(in_features, num_classes[class_name]))
 
         self.num_classes = num_classes
+        self.use_softmax = use_softmax
+
+        if self.use_softmax:
+            self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input):
         out = {}
-        for key in self.num_classes:
-            classifier = getattr(self, key)
-            out[key] = classifier(input)
+        for cls in self.num_classes:
+            classifier = getattr(self, cls)
+            out[cls] = classifier(input)
+            if self.use_softmax:
+                out[cls] = self.softmax(out[cls])
+
         return out
