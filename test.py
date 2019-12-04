53b4faa43ace32ec86ead5538b414a43f66f1372
Tridivraj Bhattacharyya <tridiv_b@yahoo.com>
2019-12-04 16:03:30 +0100

    Load pretrained weights for testing

--- test.py
+++ test.py
@@ -62,13 +62,12 @@ def test(
                     precision[cls] += prec
                     recall[cls] += rec
                     confusion_matrix[cls] += conf_mat
-                    
-            
+
             results["action_id"].extend([action_id.numpy()])
             for cls in out.keys():
-                results[cls].extend([out[cls].cpu().numpy() if out[cls].is_cuda else out[cls].numpy()])
-
-            break
+                results[cls].extend(
+                    [out[cls].cpu().numpy() if out[cls].is_cuda else out[cls].numpy()]
+                )
 
     if test_loss > 0:
         test_loss /= no_batches
@@ -83,24 +82,35 @@ def test(
 
 def run_tester(cfg, logger, modality):
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    cfg.MODEL.USE_SOFTMAX = True
 
     logger.info("Initializing model...")
     model = build_model(cfg, modality)
     logger.info("Model initialized.")
     logger.info("----------------------------------------------------------")
 
-    # if cfg.TEST.PRE_TRAINED:
-    #     pre_trained = cfg.TEST.PRE_TRAINED
-    # elif cfg.TRAIN.PRE_TRAINED:
-    #     pre_trained = cfg.TRAIN.PRE_TRAINED
-    # else:
-    #     logger.exception("No pre-trained weights provided. Please set the PRE_TRAINED paramter for either TRAIN or TEST in config file.")
-    
-    # logger.info("Loading pre-trained weights...")
-    # model.load_state_dict(torch.load(pre_trained))
-    # logger.info("Done.")
-    # logger.info("----------------------------------------------------------")
+    if cfg.TEST.PRE_TRAINED:
+        pre_trained = cfg.TEST.PRE_TRAINED
+    elif cfg.TRAIN.PRE_TRAINED:
+        pre_trained = cfg.TRAIN.PRE_TRAINED
+    else:
+        pre_trained = os.path.join(
+            cfg.DATA.OUT_DIR,
+            "checkpoints",
+            "epic_{}_{}.pth".format(cfg.MODEL.ARCH, "_".join(modality)),
+        )
+        if not os.path.exists(pre_trained):
+            logger.exception(
+                "No pre-trained weights exist. Please set the PRE_TRAINED paramter for either TRAIN or TEST in config file."
+            )
+
+    logger.info("Loading pre-trained weights {}...".format(pre_trained))
+    data_dict = torch.load(pre_trained, map_location="cpu")
+    model.load_state_dict(data_dict["state_dict"])
+    logger.info("Done.")
+    logger.info("----------------------------------------------------------")
 
     criterion = torch.nn.CrossEntropyLoss()
 
@@ -189,7 +199,6 @@ def run_tester(cfg, logger, modality):
             logger.info("Saved results to {}".format(out_file))
         except Exception as e:
             logger.exception(e)
-
 
     hours, minutes, seconds = get_time_diff(start_time, time.time())
     logger.info(
