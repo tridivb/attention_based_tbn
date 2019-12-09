Fix bug in single-mode model and dump metric calculation on gpu

models/model.py
@@ -36,14 +36,12 @@ class TBNModel(nn.Module):
             )
             self.add_module(
                 "classifier",
-                Classifier(self.num_classes, 512, use_softmax=cfg.MODEL.USE_SOFTMAX),
+                Classifier(self.num_classes, 512),
             )
         else:
             self.add_module(
                 "classifier",
-                Classifier(
-                    self.num_classes, in_features, use_softmax=cfg.MODEL.USE_SOFTMAX
-                ),
+                Classifier(self.num_classes, in_features),
             )
 
     def _create_base_model(self, modality):
@@ -94,7 +92,7 @@ class TBNModel(nn.Module):
             features.extend([feature.view(b * n, -1)])
         features = torch.cat(features, dim=1)
 
-        if self.fusion:
+        if len(self.modality) > 1:
             features = self.fusion(features)
 
         out = self.classifier(features)
@@ -139,24 +137,18 @@ class Fusion(nn.Module):
 
 
 class Classifier(nn.Module):
-    def __init__(self, num_classes, in_features, use_softmax=False):
+    def __init__(self, num_classes, in_features):
         super(Classifier, self).__init__()
 
         self.num_classes = num_classes
-        self.use_softmax = use_softmax
 
         for cls in num_classes.keys():
             self.add_module(cls, nn.Linear(in_features, self.num_classes[cls]))
-            if self.use_softmax:
-                self.add_module("{}_softmax".format(cls), nn.Softmax(dim=1))
 
     def forward(self, input):
         out = {}
         for cls in self.num_classes:
             classifier = getattr(self, cls)
             out[cls] = classifier(input)
-            if self.use_softmax:
-                softmax = getattr(self, "{}_softmax".format(cls))
-                out[cls] = softmax(out[cls])
 
         return out

tools/test.py
@@ -34,7 +34,7 @@ def test(
     results["action_id"] = []
     for cls, no_cls in cfg.MODEL.NUM_CLASSES.items():
         test_acc[cls] = [0] * (len(cfg.VAL.TOPK))
-        confusion_matrix[cls] = np.zeros((no_cls, no_cls))
+        confusion_matrix[cls] = torch.zeros((no_cls, no_cls), device=device)
         precision[cls] = 0
         recall[cls] = 0
         results[cls] = []
@@ -55,7 +55,7 @@ def test(
                 test_loss += loss.item()
                 for cls in test_acc.keys():
                     acc, conf_mat, prec, rec = metric.calculate_metrics(
-                        out[cls], target[cls], topk=cfg.VAL.TOPK
+                        out[cls], target[cls], device, topk=cfg.VAL.TOPK
                     )
                     test_acc[cls] = [x + y for x, y in zip(test_acc[cls], acc)]
                     precision[cls] += prec
@@ -74,6 +74,9 @@ def test(
             test_acc[cls] = [round(x / no_batches, 2) for x in test_acc[cls]]
             precision[cls] = round(precision[cls] / no_batches, 2)
             recall[cls] = round(recall[cls] / no_batches, 2)
+            if device.type == "cuda":
+                confusion_matrix[cls] = confusion_matrix[cls].cpu()
+            confusion_matrix[cls] = confusion_matrix[cls].numpy()
         return test_loss, test_acc, confusion_matrix, precision, recall, results
     else:
         return results

tools/train.py
@@ -70,7 +70,7 @@ def validate(
     confusion_matrix = {}
     for cls, no_cls in cfg.MODEL.NUM_CLASSES.items():
         val_acc[cls] = [0] * (len(cfg.VAL.TOPK))
-        confusion_matrix[cls] = np.zeros((no_cls, no_cls))
+        confusion_matrix[cls] = torch.zeros((no_cls, no_cls), device=device)
         precision[cls] = 0
         recall[cls] = 0
 
@@ -84,7 +84,7 @@ def validate(
             val_loss += loss.item()
             for cls in val_acc.keys():
                 acc, conf_mat, prec, rec = metric.calculate_metrics(
-                    out[cls], target[cls], topk=cfg.VAL.TOPK
+                    out[cls], target[cls], device, topk=cfg.VAL.TOPK
                 )
                 val_acc[cls] = [x + y for x, y in zip(val_acc[cls], acc)]
                 precision[cls] += prec
@@ -96,6 +96,9 @@ def validate(
         val_acc[cls] = [round(x / no_batches, 2) for x in val_acc[cls]]
         precision[cls] = round(precision[cls] / no_batches, 2)
         recall[cls] = round(recall[cls] / no_batches, 2)
+        if device.type == "cuda":
+            confusion_matrix[cls] = confusion_matrix[cls].cpu()
+        confusion_matrix[cls] = confusion_matrix[cls].numpy()
 
     return val_loss, val_acc, confusion_matrix, precision, recall
 
@@ -138,6 +141,8 @@ def run_trainer(cfg, logger, modality, writer):
         else:
             model.load_state_dict(data_dict["model"])
         optimizer.load_state_dict(data_dict["optimizer"])
+        # if lr_scheduler and "scheduler":
+        #     lr_scheduler.load_state_dict(data_dict["scheduler"])
         start_epoch = data_dict["epoch"] + 1
         train_loss_hist = data_dict["train_loss"]
         val_loss_hist = data_dict["validation_loss"]
@@ -265,9 +270,6 @@ def run_trainer(cfg, logger, modality, writer):
             cfg, model, train_loader, optimizer, criterion, modality, logger, device
         )
 
-        if lr_scheduler:
-            lr_scheduler.step()
-
         logger.info("Validation in progress...")
 
         if cfg.NUM_GPUS > 1:
@@ -284,6 +286,9 @@ def run_trainer(cfg, logger, modality, writer):
         for k in val_acc_hist.keys():
             val_acc_hist[k].append(val_acc[k])
 
+        if lr_scheduler:
+            lr_scheduler.step()
+
         if val_loss < min_val_loss:
             save_checkpoint(
                 model,
@@ -293,6 +298,7 @@ def run_trainer(cfg, logger, modality, writer):
                 val_loss_hist,
                 val_acc_hist,
                 confusion_matrix,
+                scheduler=lr_scheduler,
                 filename=checkpoint,
             )
             min_val_loss = val_loss

utils/metric.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 
 class Metric(object):
@@ -7,10 +6,10 @@ class Metric(object):
         super(Metric, self).__init__()
         pass
 
-    def calculate_metrics(self, out, target, topk=[1,]):
+    def calculate_metrics(self, out, target, device, topk=[1,]):
         maxk = max(topk)
         batch_size = out.size(0)
-        conf_mat = np.zeros((out.size(1), out.size(1)))
+        conf_mat = torch.zeros((out.size(1), out.size(1)), device=device)
 
         _, preds = out.topk(maxk, 1, True, True)
         preds = preds.t()
@@ -34,9 +33,9 @@ class Metric(object):
         recall = 0
 
         # tn = conf_mat[0, 0]
-        tp = np.trace(conf_mat[1:, 1:])
-        fn = conf_mat[1:, 0].sum()
-        fp = conf_mat[1:, 1:].sum() - tp
+        tp = conf_mat[1:, 1:].diag().sum().item()
+        fn = conf_mat[1:, 0].sum().item()
+        fp = conf_mat[1:, 1:].sum().item() - tp
 
         if tp + fp > 0:
             precision = 100 * tp / (tp + fp)