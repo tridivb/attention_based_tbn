efb2d161a8a7053aca548bc07005905c94129d15
Tridivraj Bhattacharyya <tridiv_b@yahoo.com>
2019-12-19 15:15:27 +0100

    Document code, remove unused block

--- preprocessing/create_epic_flow_pickle.py
+++ preprocessing/create_epic_flow_pickle.py
@@ -9,6 +9,10 @@ from joblib import Parallel, delayed
 
 
 def parse_args():
+    """
+    Helper function to parse command line arguments
+    """
+    
     parser = argparse.ArgumentParser(
         description="dump epic kitchens flow images into pickle files"
     )
@@ -55,6 +59,26 @@ def parse_args():
 
 
 def get_time_diff(start_time, end_time):
+    """
+    Helper function to calculate time difference
+
+    Args
+    ----------
+    start_time: float
+        Start time in seconds since January 1, 1970, 00:00:00 (UTC)
+    end_time: float
+        End time in seconds since January 1, 1970, 00:00:00 (UTC)
+
+    Returns
+    ----------
+    hours: int
+        Difference of hours between start and end time
+    minutes: int
+        Difference of minutes between start and end time
+    seconds: int
+        Difference of seconds between start and end time
+    """
+
     hours = int((end_time - start_time) / 3600)
     minutes = int((end_time - start_time) / 60) - (hours * 60)
     seconds = round((end_time - start_time) % 60)
@@ -62,6 +86,22 @@ def get_time_diff(start_time, end_time):
 
 
 def read_image(path, img_file):
+    """
+    Helper function to read image file(s)
+
+    Args
+    ----------
+    path: str
+        Source path of image file
+    img_file: str
+        Name of image file
+
+    Returns
+    ----------
+    img: np.ndarray
+        A numpy array of the image
+    """
+
     assert os.path.exists(
         os.path.join(path, "u", img_file)
     ), "{} file does not exist".format(os.path.join(path, "u", img_file))
@@ -75,15 +115,30 @@ def read_image(path, img_file):
 
 
 def integrity_check(file):
+    """
+    Helper function to check integrity of a compressed numpy file
+
+    Args
+    ----------
+    file: str
+        Absolute location of compressed numpy file
+
+    Returns
+    ----------
+    check_flag: bool
+        A flag confirming if the file is ok or not
+    """
+
+    check_flag = None
     try:
         with np.load(file) as data:
             _ = data["flow"]
             data.close()
-            return True
+        check_flag = True
     except:
         print("{} is corrupted. Overwriting file.".format(file))
-        return False
-
+        check_flag = False
+    return check_flag
 
 def save_images_to_pickle(
     record,
@@ -94,6 +149,28 @@ def save_images_to_pickle(
     file_format="frame_{:010d}.jpg",
     attempts=10,
 ):
+    """
+    Helper function to iterate over each frame of a trimmed action segment and
+    save the array of stacked flow frames to a compressed numpy file
+
+    Args
+    ----------
+    record: pd.dataframe row
+        Row with trimmed action segmentation of untrimmed video
+    root_dir: str
+        Root directory of dataset
+    out_dir: str
+        Directory to store output files
+    win_len: int
+        No of optical flow frames to stack
+    ext: str, default="jpg"
+        Extension of optical flow files
+    file_format: str, default="frame_{:010d}.jpg"
+        File naming format
+    attempts: int, default=10
+        No of attempts to write files
+
+    """
 
     vid_id = record["video_id"]
     vid_path = os.path.join(root_dir, record["participant_id"], vid_id)
@@ -109,15 +186,18 @@ def save_images_to_pickle(
         out_file = os.path.join(
             out_dir, os.path.splitext(file_format.format(idx - 1))[0] + ".npz"
         )
+        # If file exists and is ok, skip
         if os.path.exists(out_file) and integrity_check(out_file):
             full_read = True
             continue
         else:
             for a in range(attempts):
+                # Create the whole flow stack for non-sequential frame indices
                 if full_read:
                     img = []
                     for i in range(win_len):
                         img.append(read_image(vid_path, file_format.format(idx + i)))
+                # Only append the data from new flow files in case of sequential frame indices
                 else:
                     img = [img[:, :, 2:]]
                     img.append(read_image(vid_path, file_format.format(idx + win_len)))
@@ -146,15 +226,7 @@ def main(args):
         )
     )
     print("----------------------------------------------------------")
-    # for rid, r in annotations.iterrows():
-    #     save_images_to_pickle(
-    #         r,
-    #         args.root_dir,
-    #         args.out_dir,
-    #         args.win_len,
-    #         ext=args.ext,
-    #         file_format=args.file_format,
-    #     )
+    # Trimmed action segments to be processed in parallel
     results = Parallel(n_jobs=args.njobs, verbose=5)(
         delayed(save_images_to_pickle)(
             r,
