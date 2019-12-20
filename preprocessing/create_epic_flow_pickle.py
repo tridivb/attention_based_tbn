1bcb3381cc3cafe1aa731b33df2d617eb22d598c
Tridivraj Bhattacharyya <tridiv_b@yahoo.com>
2019-12-20 11:50:49 +0100

    Fix single file multi write issue

--- preprocessing/create_epic_flow_pickle.py
+++ preprocessing/create_epic_flow_pickle.py
@@ -12,7 +12,7 @@ def parse_args():
     """
     Helper function to parse command line arguments
     """
-    
+
     parser = argparse.ArgumentParser(
         description="dump epic kitchens flow images into pickle files"
     )
@@ -34,9 +34,6 @@ def parse_args():
         help="path to save output files",
         default="./epic/flow",
         type=str,
-    )
-    parser.add_argument(
-        "--ext", dest="ext", help="extension of image files", default="jpg", type=str
     )
     parser.add_argument(
         "--file-format",
@@ -110,7 +107,7 @@ def read_image(path, img_file):
         os.path.join(path, "u", img_file)
     ), "{} file does not exist".format(os.path.join(path, "v", img_file))
     v_img = cv2.imread(os.path.join(path, "v", img_file), 0)
-    img = np.concatenate((u_img[..., None], v_img[..., None]), axis=2).astype(np.uint8)
+    img = np.concatenate((u_img[..., None], v_img[..., None]), axis=2)
     return img
 
 
@@ -140,12 +137,12 @@ def integrity_check(file):
         check_flag = False
     return check_flag
 
+
 def save_images_to_pickle(
-    record,
+    annotations,
     root_dir,
     out_dir,
     win_len,
-    ext="jpg",
     file_format="frame_{:010d}.jpg",
     attempts=10,
 ):
@@ -163,8 +160,6 @@ def save_images_to_pickle(
         Directory to store output files
     win_len: int
         No of optical flow frames to stack
-    ext: str, default="jpg"
-        Extension of optical flow files
     file_format: str, default="frame_{:010d}.jpg"
         File naming format
     attempts: int, default=10
@@ -172,71 +167,78 @@ def save_images_to_pickle(
 
     """
 
-    vid_id = record["video_id"]
-    vid_path = os.path.join(root_dir, record["participant_id"], vid_id)
+    for _, record in annotations.iterrows():
+        vid_id = record["video_id"]
+        vid_path = os.path.join(root_dir, record["participant_id"], vid_id)
 
-    out_dir = os.path.join(out_dir, "flow_pickle", vid_id)
-    os.makedirs(out_dir, exist_ok=True)
+        out_dir = os.path.join(out_dir, "flow_pickle", vid_id)
+        os.makedirs(out_dir, exist_ok=True)
 
-    start_frame = max(record["start_frame"] // 2, 1)
-    end_frame = max(record["stop_frame"] // 2, 2)
+        start_frame = max(record["start_frame"] // 2, 1)
+        end_frame = max(record["stop_frame"] // 2, 2)
 
-    full_read = True
-    for idx in range(start_frame, end_frame + 1 - win_len):
-        out_file = os.path.join(
-            out_dir, os.path.splitext(file_format.format(idx - 1))[0] + ".npz"
-        )
-        # If file exists and is ok, skip
-        if os.path.exists(out_file) and integrity_check(out_file):
-            full_read = True
-            continue
-        else:
-            for a in range(attempts):
-                # Create the whole flow stack for non-sequential frame indices
-                if full_read:
-                    img = []
-                    for i in range(win_len):
-                        img.append(read_image(vid_path, file_format.format(idx + i)))
-                # Only append the data from new flow files in case of sequential frame indices
-                else:
-                    img = [img[:, :, 2:]]
-                    img.append(read_image(vid_path, file_format.format(idx + win_len)))
-                img = np.concatenate(img, axis=2)
-                np.savez_compressed(out_file, flow=img)
-                if integrity_check(out_file):
-                    full_read = False
-                    break
-                elif a == attempts - 1:
-                    print(
-                        "Unable to save {} properly. File might be corrupted".format(
-                            out_file
+        full_read = True
+        for idx in range(start_frame, end_frame + 1 - win_len):
+            out_file = os.path.join(
+                out_dir, os.path.splitext(file_format.format(idx - 1))[0] + ".npz"
+            )
+            # If file exists and is ok, skip
+            if os.path.exists(out_file) and integrity_check(out_file):
+                full_read = True
+                continue
+            else:
+                for a in range(attempts):
+                    # Create the whole flow stack for non-sequential frame indices
+                    if full_read:
+                        img = []
+                        for i in range(win_len):
+                            img.append(
+                                read_image(vid_path, file_format.format(idx + i))
+                            )
+                    # Only append the data from new flow files in case of sequential frame indices
+                    else:
+                        img = [img[:, :, 2:]]
+                        img.append(
+                            read_image(vid_path, file_format.format(idx + win_len - 1))
                         )
-                    )
+                    img = np.concatenate(img, axis=2)
+                    np.savez_compressed(out_file, flow=img)
+                    if integrity_check(out_file):
+                        full_read = False
+                        break
+                    elif a == attempts - 1:
+                        print(
+                            "Unable to save {} properly. File might be corrupted".format(
+                                out_file
+                            )
+                        )
+    print("Completed saving flow frames for {}".format(record["participant_id"]))
 
 
 def main(args):
 
     annotations = pd.read_csv(args.annotation_file)
+    participant_list = annotations["participant_id"].unique().tolist()
 
     start = time.time()
 
     print(
-        "Processing {} video annoations with {} concurrent workers".format(
-            annotations.shape[0], args.njobs
+        "Processing {} video annoations for {} participants with {} concurrent workers".format(
+            annotations.shape[0], len(participant_list), args.njobs
         )
     )
     print("----------------------------------------------------------")
-    # Trimmed action segments to be processed in parallel
-    results = Parallel(n_jobs=args.njobs, verbose=5)(
+    # Parallel processing of each participant
+    # Videos for each participant will be processed sequentially to prevent multiple writes to same file
+    results = Parallel(n_jobs=args.njobs, verbose=10)(
         delayed(save_images_to_pickle)(
-            r,
+            annotations.query("participant_id == @p"),
             args.root_dir,
             args.out_dir,
             args.win_len,
-            ext=args.ext,
             file_format=args.file_format,
         )
-        for _, r in annotations.iterrows()
+        for p in participant_list
     )
 
     print("Done")
